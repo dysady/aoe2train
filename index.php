55ca1c916739bcbd43ce5e4260e0a0a0c88f29d9
dysady <dysadgame1@gmail.com>
2024-12-24 13:47:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Training</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div id="imageContainer">
+        <img id="displayedImage" src="" alt="Image à afficher">
+    </div>
+    <div id="stat"></div>
+    <div id="correction"></div>
+    <div id="message"></div>
+    <hr>
+    <div id="current"></div>
+    <hr>
+    <a href="index.php">Page d'accueil</a>
+    <a href="trainbat.php">training batiment</a>
+    <div class="btn">
+        <button id="facile">Facile</button>
+        <button id="moyen">Moyen</button>
+        <button id="dur">Dur</button>
+        <button id="instant">Instant</button>
+    </div>
+    <div id="choice">moyen</div>
+    <script src="js/script.js"></script>
+</body>
+</html>
